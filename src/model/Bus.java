8b067187821e90adad51c3d2b880c749ef750661
Flamzyboi1 <favourobidiaso@gmail.com>
2026-03-27 15:00:41 +0200

    added set functions and constructors and to string to bus class

--- src/model/Bus.java
+++ src/model/Bus.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.security.PublicKey;
+
 public class Bus extends Vehicle{
 	//1. variables
 	private int numberofSeats;
@@ -8,23 +10,39 @@ public class Bus extends Vehicle{
 	public int getnumberofSeats() {
 		return numberofSeats;
 	}
-	public  boolean gethasBaggageDivision() {
+	public  boolean gethasBaggageDivision(boolean b) {
 	    return hasBaggageDivision;
 	    }
 	//3. setters
 	public void setnumberofSeats(int inputnumberofSeats) {
-		if(inputnumberofSeats >= 0 && (inputnumberofSeats <= 2000)) {
+		if(inputnumberofSeats >= 5 && (inputnumberofSeats <= 100)) {
 			numberofSeats = inputnumberofSeats;
 		    }
 		    else
 		    {
-		    	numberofSeats = 1;
+		    	numberofSeats = 5;
 		    } 
 		
 	    }
-	public void sethasBaggageDvision() {
+	public void sethasBaggageDvision(boolean inputhasBaggageDivision) {
+		hasBaggageDivision =inputhasBaggageDivision; 
 		
 	}
+	//4. no arg constructors
+	public Bus(String inputTitle,float inputprice,EnergyType inputetype,int inputnumberofSeats, boolean inputhasBaggageDivision) {
+	 super();// this will call vehicle() no args constructor
+	 setTitle("Tesla");// i would like to change default vehicle name 
+	 setVehicleCode();// need to call it because the title is changed
+	
+		setnumberofSeats(50);
+		gethasBaggageDivision(true);
+		
+	}
+	//6. to string
+	public String toString() {
+		String result = super.toString() +" "+numberofSeats +" " + hasBaggageDivision;
+		return result;
+    	}
 	}
 
 
