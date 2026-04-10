332bf229c7d6880cb9ae19c8088b3b81053f5621
Flamzyboi1 <favourobidiaso@gmail.com>
2026-04-10 14:19:24 +0300

    Added setters to purchase class

--- src/model/Purchase.java
+++ src/model/Purchase.java
@@ -17,6 +17,25 @@ public class Purchase {
 		return shoppinglist;
 		
 	}
+	//setters
+	public void setUserCardNo(String inputUserCardNo) {
+		if(inputUserCardNo != null && !inputUserCardNo.isEmpty()&& inputUserCardNo.matches("[A-Z{2}[0-9]{5}")) {
+			userCardNo =inputUserCardNo;
+		}
+		else {
+			userCardNo = "AB12345";
+		}
+		
+	}
+	public void setShoppingList(ArrayList<Vehicle> inputShoppingList) {
+		if(inputShoppingList != null && !inputShoppingList.isEmpty()){
+			shoppinglist =inputShoppingList;
+		}
 	
+	else 
+	{
+		shoppinglist = new ArrayList<Vehicle>();
+	}
 
+    }
 }
